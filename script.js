339dc80baabb53d767a9c143c079fde7b855acf6
MohsinRaza1214 <raza.mohsin1441@gmail.com>
2024-01-22 19:56:37 +0500

    animation & effects added on cursor

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+var crsr =  document.querySelector("#cursor");
+var blur =  document.querySelector("#cursor-blur");
+
+document.addEventListener("mousemove", function(dets) {
+    crsr.style.left = dets.x + "px";
+    crsr.style.top = dets.y + "px";
+    blur.style.left = dets.x - 250 + "px";
+    blur.style.top = dets.y - 250 + "px";
+});
+
+
 gsap.to("#nav", {
     backgroundColor: "#000",
     duration: 0.7,
