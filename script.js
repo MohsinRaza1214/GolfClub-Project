527378b56cf8086bec23c1526b09b7289614396c
MohsinRaza1214 <raza.mohsin1441@gmail.com>
2024-01-22 18:44:24 +0500

    add animation on navbar & page1 using gsap

---
+++ script.js
@@ -0,0 +1,25 @@
+gsap.to("#nav", {
+    backgroundColor: "#000",
+    duration: 0.7,
+    height: "90px",
+    scrollTrigger: {
+        trigger: "#nav",
+        scroller: "body",
+        // markers: true,
+        start: "top -10%",
+        end: "top -11%",
+        scrub: 1
+    }
+})
+
+gsap.to("#main", {
+    backgroundColor: "#000",
+    scrollTrigger: {
+        trigger: "#main",
+        scroller: "body",
+        // markers: true,
+        start: "top -25%",
+        end: "top -70%",
+        scrub: 2
+    }
+})
