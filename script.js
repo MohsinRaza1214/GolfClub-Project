763e86af2d46a6688e0bc18dc05340a2cb039512
MohsinRaza1214 <raza.mohsin1441@gmail.com>
2024-01-26 19:46:50 +0500

    some animations added on page 2 and page 3

--- script.js
+++ script.js
@@ -9,6 +9,20 @@ document.addEventListener("mousemove", function(dets) {
 });
 
 
+var h4all = document.querySelectorAll("#nav h4");
+h4all.forEach(function(elem){
+    elem.addEventListener("mouseenter", function() {
+        crsr.style.scale = 3;
+        crsr.style.border = "1px solid #fff";
+        crsr.style.backgroundColor = "transparent";
+    })
+    elem.addEventListener("mouseleave", function() {
+        crsr.style.scale = 1;
+        crsr.style.border = "0px solid #95c11e";
+        crsr.style.backgroundColor = "#95c11e";
+    })
+})
+
 gsap.to("#nav", {
     backgroundColor: "#000",
     duration: 0.7,
@@ -58,5 +72,36 @@ gsap.from("#quote2", {
         start: "top 55%",
         end: "top 45%",
         scrub: 4
+    }
+})
+
+
+gsap.from("#about-us img, #about-us-in", {
+    y: 90,
+    opacity: 0,
+    duration: 1,
+    stagger: 0.3,
+    scrollTrigger: {
+        trigger: "#about-us", 
+        scroller: "body",
+        // markers: true,
+        start: "top 75%",
+        end: "top 58%",
+        scrub: 1
+    }
+})
+
+gsap.from(".card", {
+    scale: 0.8,
+    opacity: 0,
+    duration: 1,
+    stagger: 0.3,
+    scrollTrigger: {
+        trigger: ".card", 
+        scroller: "body",
+        // markers: true,
+        start: "top 75%",
+        end: "top 58%",
+        scrub: 1
     }
 })
